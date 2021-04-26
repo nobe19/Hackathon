8e52b95e42989133bbd5126376c75ad8b2ce1abf
Nipuni Obe <obe@gmail.com>
2021-04-26 18:50:02 -0400

    end of class

--- Hackathon/TeamsListViewController.swift
+++ Hackathon/TeamsListViewController.swift
@@ -10,25 +10,41 @@ import UIKit
 class TeamsListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    var teams = ["Hodors Coders", "The Swiftettes", "Ada's Playground", "Bug Crushers"]
+    var teams: Teams!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        teams = Teams()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        teams.loadData {
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier = "ShowTeam" {
+            let destination = segue.destination as! TeamDetailTableViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.team = teams.teamArray[selectedIndexPath.row]
+        }
     }
 }
 
 extension TeamsListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return teams.count
+        return teams.teamArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = teams[indexPath.row]
+        cell.textLabel?.text = teams.teamArray[indexPath.row].teamName
         return cell
     }
-    
-    
 }
